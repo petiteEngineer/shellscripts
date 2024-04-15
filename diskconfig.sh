6ac5ebc38ed4f14e4990406acdc3b3166ff23d99
root <idestroyworlds03@gmail.com>
2024-04-14 20:02:09 -0400

    Initial Release of Shell Scripts

--- diskconfig.sh
+++ diskconfig.sh
@@ -9,5 +9,3 @@ echo -e "\n\n Partition configuration: \n\n" >>$FILENAME.report
 fdisk -l | head -17 >>$FILENAME.report
 echo -e "\n\n Mounted filesystems: \n\n" >>$FILENAME.report
 df -hT | grep -v tmp >>$FILENAME.report
-echo -e "\n\n RAID configuration: \n\n" >>$FILENAME.report
-mdadm --detail /dev/md0 >>$FILENAME.report
